a4f9c92b774cfb0046c25941f911dfe5613765da
clatot <kat.clatot@gmail.com>
2024-01-15 20:59:02 +0100

    Ajout retour fleches + fonctionnement dots

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,87 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-let leftArrow = document.querySelector("#banner .arrow_left")
+let slideNumber = 0;
 
+let leftArrow = document.querySelector("#banner .arrow_left")
 let rightArrow = document.querySelector("#banner .arrow_right") 
+let dotsNumber = slides.length
+let dotsParent = document.querySelector("#banner .dots")
+let slideImage = document.querySelector("#banner .banner-img")
+let tagLineTitle = document.querySelector("#banner p")
+
+for (let i = 0; i < dotsNumber; i++) {
+	let dot = document.createElement("span")
+	dot.classList.add("dot")
+	dotsParent.appendChild(dot)
+}
+
+let dots = document.querySelectorAll("#banner .dot")
+console.log(dots)
 
 leftArrow.addEventListener("click", function () {
+
+	if (slideNumber === 0) {
+		slideNumber = (slides.length -1)
+	} else {
+		slideNumber --
+	}
+
+	for (let i = 0; i < slides.length; i++) {
+		dots[i].classList.remove("dot_selected")
+	}
+
+	dots[slideNumber].classList.add("dot_selected")
+	slideImage.src = slides[slideNumber].image
+	tagLineTitle.innerHTML = slides[slideNumber].tagLine
+
 	console.log("Fleche gauche cliquée.")
+	console.log(slideNumber)
 })
 
 rightArrow.addEventListener("click", function () {
+
+	if (slideNumber === (slides.length -1)) {
+		slideNumber = 0
+	} else {
+		slideNumber ++
+	}
+	
+	for (let i = 0; i < slides.length; i++) {
+		dots[i].classList.remove("dot_selected")
+	}
+	
+	dots[slideNumber].classList.add("dot_selected")
+	slideImage.src = slides[slideNumber].image
+	tagLineTitle.innerHTML = slides[slideNumber].tagLine
+
 	console.log("Fleche droite cliquée.")
+	console.log(slideNumber)
 })
+
+
+// console.log(dots)
+// dots[slideNumber].classList.add("dot_selected")
+
+// function fonctionChangementSlide(change) {
+// 	i 
+// }
+
+console.log(slideNumber)
+
